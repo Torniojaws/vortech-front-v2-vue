1d330e826792136b7b6b5d0aa2b15360734f0264
Juha Untinen <torniojaws@gmail.com>
2019-10-23 22:37:47 +0300

    NewsItem mock up

--- vortech-frontend/src/components/News.vue
+++ vortech-frontend/src/components/News.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="news">
     Here will be {{ count }} news
+    <template v-for="item in demoNews">
+      <NewsItem :news="item" :key="item.id" />
+    </template>
   </div>
 </template>
 
 <script>
+import NewsItem from '@/components/NewsItem.vue'
+
 export default {
   name: 'News',
+  components: {
+    NewsItem
+  },
+  data () {
+    return {
+      // IRL this would be populated by calling the backend REST API
+      demoNews: [{
+        id: 1,
+        title: 'Example news',
+        contents: 'This would be a news item from the DB',
+        created: '2019-10-23 00:18:00'
+      }, {
+        id: 2,
+        title: 'More news',
+        contents: 'Another news item from the DB',
+        created: '2019-10-23 00:19:00'
+      }]
+    }
+  },
   props: {
     count: String
   }
---
+++ vortech-frontend/src/components/NewsItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="newsItem">
+    <header>
+      <h2>{{ news.title }}</h2>
+      <aside>{{ news.created }}</aside>
+    </header>
+    <section>
+      {{ news.contents }}
+    </section>
+    <footer>
+      Comments, likes, etc, for news.id: {{ news.id }}
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewsItem',
+  props: {
+    news: Object
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background-color: rgb(48, 58, 66);
+}
+.newsItem {
+  text-align: left;
+}
+</style>
